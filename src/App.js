2d294864c6958c3f9e985209a511f064d2eadc43
Melissa McGuire <74629223+maggiemadison@users.noreply.github.com>
2021-12-09 03:17:53 -0600

    added subscription to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'antd/dist/antd.css'
 import { v4 as uuid } from 'uuid'
 import { List, Input, Button } from 'antd'
 import { listNotes } from './graphql/queries'
+import { onCreateNote } from './graphql/subscriptions'
 import {
   updateNote as UpdateNote,
   createNote as CreateNote,
@@ -112,6 +113,17 @@ const App =()  => {
 
     useEffect(() => {
       fetchNotes();
+      const subscription = API.graphql({
+        query: onCreateNote
+      })
+        .subscribe({
+          next: noteData => {
+            const note = noteData.value.data.onCreateNote
+            if (CLIENT_ID === note.clientId) return
+            dispatch({ type: 'ADD_NOTE', note })
+          }
+        })
+      return () => subscription.unsubscribe()
     }, []);
 
   const styles = {
